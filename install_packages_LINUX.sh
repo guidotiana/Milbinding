3c89d027ca3785c44350ef4a44f8410738e5aba6
Riccardo Beccaria <87329576+RiccardoBecca@users.noreply.github.com>
2024-04-29 11:14:24 +0200

    Create install_packages_LINUX.sh

---
+++ install_packages_LINUX.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ~/.bashrc
+conda create -y --name Milbinding
+conda activate Milbinding
+conda install -y python=3.10.9
+conda install -y numpy=1.22.3
+pip install scikit-learn
+conda install -y matplotlib=3.7.1
+conda install -y seaborn
+conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=10.2 -c pytorch
+conda install -y pyg -c pyg
+pip install e3nn
+pip install torch-cluster 
+pip install torch-scatter 
+pip install mdtraj
+pip install notebook
