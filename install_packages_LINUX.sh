0b4b0a987e093531c7a6563bcb581fac7340a115
Riccardo Beccaria <87329576+RiccardoBecca@users.noreply.github.com>
2024-10-04 11:11:34 +0200

    Update install_packages_LINUX.sh

--- install_packages_LINUX.sh
+++ install_packages_LINUX.sh
@@ -7,7 +7,7 @@ conda install -y numpy=1.22.3 # new 1.26.4
 # scikit-learn version 1.2.2 #new 1.4.2
 pip install scikit-learn
 conda install -y matplotlib=3.7.1 # new 3.8.4
-conda install -y seaborn
+conda install -y seaborn #new 0.13.2
 conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=10.2 -c pytorch
 conda install -y pyg -c pyg
 pip install e3nn
