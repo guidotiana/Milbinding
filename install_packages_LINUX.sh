3a9183d1254e5b4a4a6858acd1010c79ed49ea3c
Riccardo Beccaria <87329576+RiccardoBecca@users.noreply.github.com>
2024-10-04 10:59:17 +0200

    Update install_packages_LINUX.sh

--- install_packages_LINUX.sh
+++ install_packages_LINUX.sh
@@ -2,11 +2,11 @@
 source ~/.bashrc
 conda create -y --name Milbinding
 source activate Milbinding
-conda install -y python=3.10.9
-conda install -y numpy=1.22.3
-# scikit-learn version 1.2.2
+conda install -y python=3.10.9 #new 3.9.19
+conda install -y numpy=1.22.3 # new 1.26.4
+# scikit-learn version 1.2.2 #new 1.4.2
 pip install scikit-learn
-conda install -y matplotlib=3.7.1
+conda install -y matplotlib=3.7.1 # new 3.8.4
 conda install -y seaborn
 conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=10.2 -c pytorch
 conda install -y pyg -c pyg
